c122b52c42e2681c7abe367b029c293570b0246d
HakkaNgin <hurinturin123@gmail.com>
2024-08-10 02:08:48 +0700

    Comment out nltk stopwords for debudding

--- resume_parser.py
+++ resume_parser.py
@@ -7,7 +7,7 @@ import nltk
 # Download required NLTK data files
 nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
-nltk.download('stopwords')
+# nltk.download('stopwords')
 nltk.download('wordnet')
 
 app = Flask(__name__)
