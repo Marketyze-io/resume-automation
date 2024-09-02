605051eb09c929577420853be0a680494fc78bdd
HakkaNgin <hurinturin123@gmail.com>
2024-09-02 21:42:47 +0700

    Modify GPT model to GPT-4-Turbo

--- resume_parser.py
+++ resume_parser.py
@@ -160,8 +160,8 @@ def extract_info_from_resume(file_path):
         try:
             # Make a call to the OpenAI API
             response = openai.completions.create(
-                # model="gpt-3.5-turbo-instruct",
-                model="gpt-4",
+                model="gpt-3.5-turbo-instruct",
+                # model="gpt-4",
                 # model="text-davinci-003",
                 prompt=prompt,
                 max_tokens=200,
